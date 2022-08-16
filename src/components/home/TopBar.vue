63783f6f5d79440ffed801ce54667a2d85ccec6f
Wasiq Bin Zahid <wasiqibnzahid@gmail.com>
2022-08-16 21:51:32 +0500

    update changes

---
+++ src/components/home/TopBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container topbar flex align-center">
+    <div class="flex align-center">
+      <div class="img-container">
+        <img src="../../assets/img/threedots.png" alt="" />
+      </div>
+      <h4 class="text-white">AUTO-TUNE</h4>
+    </div>
+    <nav class="flex align-center">
+      <router-link v-for="(item, index) in navLinks" :key="index" :to="item.to">
+        {{ item.text }}
+      </router-link>
+    </nav>
+    <div class="search flex align-center justify-center">
+      <div class="img-container">
+        <img src="../../assets/img/threedots.png" alt="" />
+      </div>
+      <div class="img-container">
+        <img src="../../assets/img/threedots.png" alt="" />
+      </div>
+      <div class="img-container">
+        <img src="../../assets/img/threedots.png" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navLinks: [
+        { text: "Home", to: "/" },
+        { text: "Products", to: "/" },
+        { text: "Learning Center", to: "/" },
+        { text: "Community", to: "/" },
+        { text: "Support", to: "/" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="home-container">
+      <div class="user-img-container"></div>
+      <div class="bg-container-home"></div>
+      <div class="home-content-box flex flex-column">
+        <TopBar />
+        <div class="home-content container">
+          <h2>Why partner with Auto-Tune?</h2>
+          <div class="flex align-center">
+            <div class="down-arrow">&#8595;</div>
+            <p>Scroll down and learn</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <firstPerson />
+    <secondPerson />
+    <thirdPerson />
+    <SubscriptionsSection />
+    <PluginsSection />
+    <FAQSection />
+    <AffiliateSection />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import TopBar from "../components/home/TopBar.vue";
+import firstPerson from "../components/home/firstPerson.vue";
+import secondPerson from "../components/home/secondPerson.vue";
+import thirdPerson from "../components/home/thirdPerson.vue";
+import PluginsSection from "../components/home/PluginsSection.vue";
+import SubscriptionsSection from "../components/home/SubscriptionsSection.vue";
+import AffiliateSection from "../components/home/AffiliateSection.vue";
+import FAQSection from "../components/home/FAQSection.vue";
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
-  }
-}
+    TopBar,
+    firstPerson,
+    secondPerson,
+    thirdPerson,
+    SubscriptionsSection,
+    PluginsSection,
+    FAQSection,
+    AffiliateSection,
+  },
+  mounted() {},
+  data() {
+    return {
+      lastPosition: 0,
+      showMyItem: true,
+    };
+  },
+  methods: {},
+};
 </script>
