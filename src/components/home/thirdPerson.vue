25c6f164225ceb308af754dfde59c5470a60806f
nayab shamshad <nayabshamshad90@gmaail.com>
2022-08-17 05:36:38 -0700

    update layout style

--- src/components/home/thirdPerson.vue
+++ src/components/home/thirdPerson.vue
@@ -1,9 +1,33 @@
 <template>
-  <div></div>
+  <div class="section person">
+    <div class="first-part">
+      <h2 class="title">Backstage Pass</h2>
+      <h3 class="subtitle">In Da Club</h3>
+
+      <div class="info">
+        <h5>Auto-Tune Affiliates have preferred access to the Auto-Tune Marketing materials and an inside track to new
+          product releases. You’ll hear it here first—and be able to turn that access into income. </h5>
+        <ul>
+          <li>Enjoy access to marketing materials to help drive your affiliate sales.</li>
+          <li>Get prizes for your competitions and giveaways.
+          </li>
+           <li>Get all of the latest Antares software for FREE
+          </li>
+        </ul>
+      </div>
+      <div class="pics">
+        <div class="pic">
+          <img src="../../assets/img/brand_pic3.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+</style>
